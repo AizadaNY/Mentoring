2db62df3811a7dd2d649c365d8e3e42a227c91a8
AizadaNY <aizada.abdyrakhmanova@gmail.com>
2023-11-19 12:57:04 -0500

    Initial commit

---
+++ src/main/java/FibonacciNumber.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+public class FibonacciNumber {
+
+    public static void main(String[] args) {
+        int num = Methods.getUserInputNumbers();
+        Methods.getFibonacciNumberUsingLoop(num);
+
+
+    }
+
+//    @Test
+//    public void test1(){
+//        int num = Methods.getUserInputNumbers();
+//        Methods.getFibonacciNumberUsingLoop(num);
+//    }
+
+
+
+}
---
+++ src/main/java/Methods.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Methods {
+    //user provides any number
+    public static int getUserInputNumbers () {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Please provide any number ");
+        int number = 0;
+        try {
+            number = userInput.nextInt();
+            System.out.println("Thanks for your input");
+            if (number == 0) {
+                System.out.println("Please provide any number except 0");
+                number = userInput.nextInt();
+                System.out.println("Thanks for your input");
+            }
+        } catch (Exception e) {
+            System.out.println("Special characters or letters not allowed to enter");
+        }
+        return number;
+    }
+
+
+    public static void getFibonacciNumberUsingLoop ( int number){
+        List<Integer> list = new ArrayList<>();
+        list.add(0);
+        list.add(1);
+        for (int i = 0; i < number - 2; i++) {
+            int lastNum =list.get(list.size()-1);
+            int secondLastNum =list.get(list.size() - 2);
+            int fibNumber = lastNum + secondLastNum;
+            list.add(fibNumber);
+
+        }
+        if (number == 0) {
+            System.out.println("No data to show based on your request");
+        } else if (number == 1) {
+            System.out.println("List of fibonacci numbers based on your request: " + list.get(0));
+        } else {
+            System.out.println("List of fibonacci numbers based on your request: " + list);
+        }
+
+    }
+
+//    public static void getFibonacciNumberUsingRecursion(int number){
+//
+//        List<Integer> list = new ArrayList<>();
+//        list.add(0);
+//        list.add(1);
+//        if(number<=1)
+//         return   System.out.println("List of fibonacci numbers based on your request: " + list.get(0));
+//        else
+//            int lastNum =list.get(list.size()-1);
+//            int secondLastNum =list.get(list.size() - 2);
+//            int fibNumber = lastNum + secondLastNum;
+//            list.add(fibNumber);
+//        System.out.println("List of fibonacci numbers based on your request: " + list);
+//
+//        getFibonacciNumberUsingRecursion(number);
+//
+//
+//
+//
+//    }
+
+}
