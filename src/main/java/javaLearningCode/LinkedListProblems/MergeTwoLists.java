a9778e52a1e195e2dfdf4238ad29c0fe97fe2683
AizadaNY <aizada.abdyrakhmanova@gmail.com>
2024-01-18 21:49:45 -0500

    linked list

--- src/main/java/javaLearningCode/LinkedListProblems/MergeTwoLists.java
+++ src/main/java/javaLearningCode/LinkedListProblems/MergeTwoLists.java
@@ -2,10 +2,22 @@ package javaLearningCode.LinkedListProblems;
 
 public class MergeTwoLists {
 
-    public static void main(String[] args) {
+    public LinkedList mergeTwoLists(LinkedList list1,LinkedList list2){
 
+        if(list1!=null&&list2!=null){
+            if(list1.value<= list2.value){
+                list1.next=mergeTwoLists(list1.next, list2);
+                return list1;
+            }else{
+                list2.next=mergeTwoLists(list1, list2.next);
+                return list2;
+            }
+        }
 
+        if(list1==null){
+            return list2;
+        }
 
-
+            return list1;
     }
 }
