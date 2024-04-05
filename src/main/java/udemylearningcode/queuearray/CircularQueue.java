7f120ce5f4231bcbaf49574451c63e1b91d5307b
AizadaNY <aizada.abdyrakhmanova@gmail.com>
2024-04-04 23:32:10 -0400

    circular queue

---
+++ src/main/java/udemylearningcode/queuearray/CircularQueue.java
@@ -0,0 +1,30 @@
+package udemylearningcode.queuearray;
+
+import java.lang.reflect.Array;
+
+public class CircularQueue {
+
+    int arr[];
+    int size;
+    int beginningOfQueue;
+    int topOfTheQueue;
+
+    public CircularQueue(int size){
+        this.arr=new int[size];
+        this.size=size;
+        this.beginningOfQueue=-1;
+        this.topOfTheQueue=-1;
+    }
+
+    public void isEmpty(int value){
+
+    }
+
+    public void isFull(int value){
+
+    }
+
+    public void enQueue(int value){
+
+    }
+}
