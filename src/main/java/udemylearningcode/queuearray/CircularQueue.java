5dbd977606c1ad2b5262da60fe5d908939bc57bc
AizadaNY <aizada.abdyrakhmanova@gmail.com>
2024-04-11 23:04:18 -0400

    queue

--- src/main/java/udemylearningcode/queuearray/CircularQueue.java
+++ src/main/java/udemylearningcode/queuearray/CircularQueue.java
@@ -9,22 +9,48 @@ public class CircularQueue {
     int beginningOfQueue;
     int topOfTheQueue;
 
-    public CircularQueue(int size){
-        this.arr=new int[size];
-        this.size=size;
-        this.beginningOfQueue=-1;
-        this.topOfTheQueue=-1;
+    public CircularQueue(int size) {
+        this.arr = new int[size];
+        this.size = size;
+        this.beginningOfQueue = -1;
+        this.topOfTheQueue = -1;
+        System.out.println("Circular queue successfully created with size " + size);
     }
 
-    public void isEmpty(int value){
-
+    public boolean isEmpty(int value) {
+        if (topOfTheQueue == -1) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    public void isFull(int value){
-
+    public boolean isFull(int value) {
+        if (topOfTheQueue + 1 == beginningOfQueue) {
+            return true;
+        } else if (beginningOfQueue == 0 & topOfTheQueue + 1 == size) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    public void enQueue(int value){
-
+    public void enQueue(int value) {
+        if(isFull(value)){
+            System.out.println("Circular Queue is full");
+        }else if(isEmpty(value)){
+            beginningOfQueue=0;
+            topOfTheQueue++;
+            arr[topOfTheQueue]=value;
+            System.out.println("Value successfully inserted");
+        }else{
+            if(topOfTheQueue+1==size){
+                topOfTheQueue=0;
+            }else{
+                topOfTheQueue++;
+            }
+            arr[topOfTheQueue]=value;
+            System.out.println("Value successfully inserted");
+        }
     }
 }
