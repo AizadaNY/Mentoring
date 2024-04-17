94b89f7cb5452ec0fff9a7f28eb8bd89e2e265b4
AizadaNY <aizada.abdyrakhmanova@gmail.com>
2024-04-16 23:21:27 -0400

    cq

--- src/main/java/udemylearningcode/queuearray/Main.java
+++ src/main/java/udemylearningcode/queuearray/Main.java
@@ -3,11 +3,18 @@ package udemylearningcode.queuearray;
 public class Main {
 
     public static void main(String[] args) {
-        QueueArray queueArray=new QueueArray(3);
-        queueArray.enQueue(3);
-        queueArray.enQueue(5);
-        queueArray.enQueue(7);
-        queueArray.enQueue(8);
+//        QueueArray queueArray=new QueueArray(3);
+//        queueArray.enQueue(3);
+//        queueArray.enQueue(5);
+//        queueArray.enQueue(7);
+//        queueArray.enQueue(8);
+//
+        CircularQueue cq=new CircularQueue(4);
+        cq.enQueue(56);
+        cq.enQueue(76);
+        cq.enQueue(100);
+        cq.enQueue(123);
+        cq.enQueue(123);
     }
 
 
